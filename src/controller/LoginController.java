0eea6749b7283afb4942198894060c044506b1c1
leilei <973250701@qq.com>
2020-02-01 16:26:52 +0800

    first commit

---
+++ src/controller/LoginController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception{
+        return new ModelAndView("login.jsp");
+    }
+}
